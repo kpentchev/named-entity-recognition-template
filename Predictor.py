bfff0fff5679f13a471158e18642887c80e26dc8
Konstantin Pentchev <kpentchev@Admin-01Nov18s-MacBook-Pro.local>
2019-03-22 13:40:09 +0100

    Predictor uses new model

--- Predictor.py
+++ Predictor.py
@@ -1,9 +1,6 @@
 import tensorflow as tf
-from nltk import download, sent_tokenize, word_tokenize, pos_tag
-from nltk.stem.snowball import SnowballStemmer
-from WordIndex import WordIndex
-from Preprocessor import encodeSentences, encodeStems, encodeChars, pad
-from StemCharEmbLstmCrfModel import restore
+from nltk import download, sent_tokenize
+from NerModel import restore
 
 MAX_LEN = 75
 MAX_LEN_CHARS = 15
@@ -15,21 +12,7 @@ class Predictor(object):
         download('averaged_perceptron_tagger')
         download('stopwords')
 
-        self.stemmer = SnowballStemmer("english", ignore_stopwords=True)
-
-        self.wordIndex = WordIndex("UNK")
-        self.wordIndex.load('models_active/word_to_index.pickle')
-
-        self.tagIndex = WordIndex("O")
-        self.tagIndex.load('models_active/tag_to_index.pickle')
-
-        self.charIndex = WordIndex("UNK")
-        self.charIndex.load('models_active/char_to_index.pickle')
-
-        self.stemIndex = WordIndex("UNK")
-        self.stemIndex.load('models_active/stem_to_index.pickle')
-
-        self.model = restore('models_active/stem_char_lstm_crf.h5')
+        self.model = restore('/Users/kpentchev/data/models/2019_03_22_12_31_stem_char_lstm_crf.h5')
         self.graph = tf.get_default_graph()
 
     def predict(self, text):
@@ -38,18 +21,9 @@ class Predictor(object):
         result = []
 
         for sentence in sentences:
-            words = pos_tag(word_tokenize(sentence))
-            encodedInput = encodeSentences([words], self.wordIndex)
-            encodedInput = pad(encodedInput, MAX_LEN, self.wordIndex.getPadIdx())
-
-            encodedStems = encodeStems([[w[0] for w in words]], self.stemIndex, self.stemmer)
-            encodedStems = pad(encodedStems, MAX_LEN, self.stemIndex.getPadIdx())
-
-            encodedChars = encodeChars([words], self.charIndex, MAX_LEN, MAX_LEN_CHARS)
-
             with self.graph.as_default():
-                prediction = self.model.predict(encodedInput, encodedStems, encodedChars)
-                for w, pred1 in zip(words, prediction[0]):
-                    result.append((w[0], self.tagIndex.getWord(pred1)))
+                prediction = self.model.predict(sentence)
+                for w, pred1 in prediction:
+                    result.append((w[0], pred1))
         
         return result
